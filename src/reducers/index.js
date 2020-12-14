fc76bd6741b6a05bcb18ad7e3cb53621426887cf
Nikolay <samoil.nikolai@gmail.com>
2020-12-14 16:38:02 +0200

    store enhancer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -11,6 +11,4 @@ const reducer = (state, action) => {
    
 };
 
-
-
 export default reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,41 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+// enhancer - imbunatațitor
+const logEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        console.log(action.type);
+        return originalDispatch(action);
+    }
+
+    return store;
+};
+
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+
+        //added 'string' support in dispatch
+        if( typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            });
+        }
+
+        return originalDispatch(action);
+    }
+
+    return store;
+};
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+
+//testing stringEnhacer
+store.dispatch('HELLO_WORLD');
 
 export default store;
